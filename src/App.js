57a4069000e752ffae0912bb9ebabf4f6bfb0372
Ankush <ankushkumar747474@gmail.com>
2023-02-02 23:30:33 +0530

    abcd

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import {useEffect,useState} from 'react';
+import './App.css';
+
+function App() {
+  const[quote,setQuote]=useState("")
+  const getQuote=()=> {
+    fetch("https://api.quotable.io/random")
+     .then((response)=>response.json())
+    .then((data)=>setQuote(data))
+  }
+  
+function changeColor() {
+  document.body.style.backgroundColor=
+  'rgb('+Math.round(Math.random()*255)+
+  ','+Math.round(Math.random()*255)+','
+    +Math.round(Math.random()*255)+')';
+}
+
+  useEffect(()=>getQuote(),[])
+  return (
+    <div className="App" >
+      <span>{quote.content}</span>
+      <h4>{quote.author}</h4>
+      <button className='btn' onClick={getQuote}{...changeColor()}>Get Quote</button>
+    </div>
+  );
+}
+
+export default App;
